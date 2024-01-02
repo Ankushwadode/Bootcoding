30d910a1bfdb2508c598e97969212cfd4aaa5547
Ankushwadode <ankushwadode154@gamil.com>
2024-01-02 13:37:10 +0530

    commit

---
+++ carsInfo/src/main/java/org/example/writer/pdf/JsonWriter.java
@@ -0,0 +1,19 @@
+package org.example.writer.pdf;
+
+import org.example.writer.IFileWriter;
+
+import java.util.List;
+
+public class JsonWriter implements IFileWriter {
+
+    private ObjectM
+
+    @Override
+    public void write(List data, String outputPath) {
+        try{
+
+        }catch (){
+
+        }
+    }
+}
