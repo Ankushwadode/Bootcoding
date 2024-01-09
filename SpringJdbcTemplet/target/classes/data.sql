90442e07b51f353c6594e070ec2f9139b842cebd
Ankushwadode <Ankushwadode154@gmail.com>
2024-01-09 12:13:08 +0530

    jdbcTemplate implementation

---
+++ SpringJdbcTemplet/target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into student (id,name,tech) values (101,'Ankush','DevOps');
+insert into student (id,name,tech) values (102,'Ankush wadode','Java');
+insert into student (id,name,tech) values (103,'Tanmay wadode','React');
