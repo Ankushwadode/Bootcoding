ea17e7f2b0579b9132dbb66ebb3dd1b2b2d98fc0
Ankushwadode <Ankushwadode154@gmail.com>
2024-01-12 09:40:02 +0530

    commit

--- SpringBootFullTutorial/src/main/java/org/bootcoding/SpringBootFullTutorialApplication.java
+++ SpringBootFullTutorial/src/main/java/org/bootcoding/SpringBootFullTutorialApplication.java
@@ -8,6 +8,7 @@ public class SpringBootFullTutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFullTutorialApplication.class, args);
+		System.out.println("running....");
 	}
 
 }
