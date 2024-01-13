4dc3d4b3725a701cad331f6a24135d1b0a296136
Ankushwadode <Ankushwadode154@gmail.com>
2024-01-13 09:07:45 +0530

    entity

--- SpringBootFullTutorial/src/main/java/org/bootcoding/entity/Department.java
+++ SpringBootFullTutorial/src/main/java/org/bootcoding/entity/Department.java
@@ -1,4 +1,26 @@
 package org.bootcoding.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long departmentId;
+
+    @NotBlank(message = "Please Provide Department Name.")
+
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
 }
