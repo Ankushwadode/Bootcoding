1bbd144887b3cfb260c7a74861ba6f67d86b6bd6
Ankushwadode <Ankushwadode154@gmail.com>
2024-01-16 09:14:12 +0530

    custom exception response entity

---
+++ SpringBootFullTutorial/src/main/java/org/bootcoding/customException/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,8 @@
+package org.bootcoding.customException;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+}
